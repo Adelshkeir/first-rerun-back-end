17b88450ce816583ef8552488e5ef017e397c474
mehioitani <muhieddineitani04@gmail.com>
2023-12-24 15:58:33 +0200

    fixed product controller to include category name

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,90 +1,88 @@
 // import Category from '../models/categoryModel.js';
-import asyncHandler from 'express-async-handler';
-import { Category } from '../models/relations.js'
-import { Product } from '../models/relations.js';
+import asyncHandler from "express-async-handler";
+import { Category } from "../models/relations.js";
+import { Product } from "../models/relations.js";
 
 // @desc    Get categories
 // @route   GET /api/categories
 // @access  Private
 export const getAllCategories = asyncHandler(async (req, res) => {
-    const category = await Category.findAll({
-        include: [{
-            model: Product,
-        }],            order: [["id", "DESC"]] 
-
-    })
-    res.status(200).json(category)
-})
+  const category = await Category.findAll({
+    order: [["id", "DESC"]],
+  });
+  res.status(200).json(category);
+});
 
 // @desc    Get one category
 // @route   GET /api/categories/:id
 // @access  Private
 export const getOneCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params
-    const category = await Category.findByPk((id), {
-        include: [{
-            model: Product,
-        }]
-    })
-    if (!category) {
-        res.status(400)
-        throw new Error('Cannot find category')
-    }
-    res.status(200).json(category)
-})
-
+  const { id } = req.params;
+  const category = await Category.findByPk(id, {
+    order: [["id", "DESC"]],
+  });
+  if (!category) {
+    res.status(400);
+    throw new Error("Cannot find category");
+  }
+  res.status(200).json(category);
+});
 
 // @desc    Create category
 // @route   POST /api/categories
 // @access  Private
 export const createCategory = asyncHandler(async (req, res) => {
-    const image = req.file
-    const { category_name, date } = req.body
-    if (!category_name || !date) {
-        res.status(400)
-        throw new Error('all fields are required')
-    }
-    const createdCategory = await Category.create({ ...req.body, category_image: image.path })
-    res.status(200).json(createdCategory)
-})
+  const image = req.file;
+  const { category_name, date } = req.body;
+  if (!category_name || !date) {
+    res.status(400);
+    throw new Error("all fields are required");
+  }
+  const createdCategory = await Category.create({
+    ...req.body,
+    category_image: image.path,
+  });
+  res.status(200).json(createdCategory);
+});
 
 // @desc    Update category
 // @route   PUT /api/categories/:id
 // @access  Private
 export const updateCategory = asyncHandler(async (req, res) => {
-    const image = req.file
-    const { id } = req.params
-    const category = await Category.findByPk(id)
-console.log(req)
-    if (!category) {
-        res.status(400)
-        throw new Error('Category not found')
-    }
+  const image = req.file;
+  const { id } = req.params;
+  const category = await Category.findByPk(id);
+  console.log(req);
+  if (!category) {
+    res.status(400);
+    throw new Error("Category not found");
+  }
 
-    if(!image) {
-        await Category.update({ ...req.body}, { where: { id: id } })
-        const updatedCategory = await Category.findByPk(id)
-        return res.status(200).json(updatedCategory)
-    }
+  if (!image) {
+    await Category.update({ ...req.body }, { where: { id: id } });
+    const updatedCategory = await Category.findByPk(id);
+    return res.status(200).json(updatedCategory);
+  }
 
-    await Category.update({ ...req.body, category_image: image.path }, { where: { id: id } })
-    const updatedCategory = await Category.findByPk(id)
-    res.status(200).json(updatedCategory)
-
-})
-
+  await Category.update(
+    { ...req.body, category_image: image.path },
+    { where: { id: id } }
+  );
+  const updatedCategory = await Category.findByPk(id);
+  res.status(200).json(updatedCategory);
+});
 
 // @desc    Delete category
 // @route   DELETE /api/categories/:id
 // @access  Private
 export const deleteCategory = asyncHandler(async (req, res) => {
-    const { id } = req.params
-    const category = await Category.findByPk(id)
+  const { id } = req.params;
+  const category = await Category.findByPk(id);
 
-    if (!category) {
-        res.status(400)
-        throw new Error('Category not found')
-    }
-    await category.destroy()
-    res.status(200).json(category)
-})
+  if (!category) {
+    res.status(400);
+    throw new Error("Category not found");
+  }
+  await category.destroy();
+  res.status(200).json(category);
+});
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-import express from "express"
+import express from "express";
 import sequelize from "./database-connection.js";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
-import cors from 'cors'
-import categoryRouter from '../first-rerun-back-end/routes/categoryRoute.js'
-import productRouter from './routes/productRoute.js'
+dotenv.config();
+import cors from "cors";
+import categoryRouter from "../first-rerun-back-end/routes/categoryRoute.js";
+import productRouter from "./routes/productRoute.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-const app = express()
-app.use(express.static('./'))
+const app = express();
+app.use(express.static("./"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-app.use('/api', categoryRouter)
-app.use('/api', productRouter)
+app.use("/api", categoryRouter);
+app.use("/api", productRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 // app.use(
 //     cors({
@@ -34,19 +34,15 @@ app.use(errorHandler)
 //     })
 //   );
 
-
 sequelize.sync({ force: false });
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
-
-
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 server.listen(process.env.PORT, () => {
-
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
